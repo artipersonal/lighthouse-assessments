5a9ebbb9cc58f5e164937a4c5f1d522f0518ab66
Vladimir Samsonov <arti_personal@protonmail.com>
2023-02-18 20:28:11 +0000

    even&hello

---
+++ even.js
@@ -0,0 +1,16 @@
+const isEven = function (num) {
+  return num % 2 === 0;
+}
+
+const tenIsEven = isEven(10);
+const elevenIsEven = isEven(11);
+
+console.log(tenIsEven);
+console.log(elevenIsEven);
+
+const isEven2 = function (num) {
+  return num % 2 === 0;
+}
+
+console.log(isEve2(10));
+console.log(isEven2(11));
