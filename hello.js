7887168499fc8a17f543a0eec60c5eeded1ea224
Vladimir Samsonov <arti_personal@protonmail.com>
2023-02-18 20:33:12 +0000

    odd

--- hello.js
+++ hello.js
@@ -10,3 +10,14 @@ const sayHello2 = function (name) {
 sayHello2("Caliban");
 sayHello2("Miranda");
 sayHello2("Ferdinand");
+
+const sayHelloToConsole = function (name) {
+  console.log("Hello, " + name);
+}
+sayHelloToConsole('John');
+
+const returnSayHello = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+console.log(greeting);
