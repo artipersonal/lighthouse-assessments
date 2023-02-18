582689f4374750a5820b740ce514582e97826c32
Vladimir Samsonov <arti_personal@protonmail.com>
2023-02-18 19:51:58 +0000

    loops

---
+++ chorus-while.js
@@ -0,0 +1,18 @@
+const chorus = "Let's dance!";
+let repeat = 0;
+while (repeat < 10) {
+  console.log(chorus);
+  repeat++;
+}
+console.log("Until the sun comes up!");
+
+//const chorus = "Let's dance!";
+repeat = 0;
+while (repeat < 10) {
+  if (repeat === 5) {
+    console.log("OH, YEAH!");
+  }
+  console.log(chorus);
+  repeat++;
+}
+console.log("Until the sun comes up!");
